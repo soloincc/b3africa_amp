026efaa021604f36de6fe1113869599db5ee14b6
Wangoru Kihara <soloincc@gmail.com>
2017-11-29 18:57:01 +0100

    Print messages when executing the script
    Signed-off-by: Wangoru Kihara <soloincc@gmail.com>

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -8,7 +8,13 @@ python /opt/azizi_amp/manage.py makemigrations b3africa
 python /opt/azizi_amp/manage.py makemigrations vendor
 python /opt/azizi_amp/manage.py migrate
 python /opt/azizi_amp/manage.py migrate --run-syncdb
+
+echo "Create the django super user"
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'info@badili.co.ke', 'Innovate254')" | python /opt/azizi_amp/manage.py shell
+
+echo "Create a default mapped database and import the schema"
 mysql -u root -padmin -h azizi-amp-db -e "create database axgg"
 mysql -u root -padmin -h azizi-amp-db axgg < /opt/azizi_amp/axgg.sql
+
+echo "Start the server"
 python /opt/azizi_amp/manage.py runserver 0.0.0.0:8089
